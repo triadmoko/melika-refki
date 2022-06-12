b6333c6003f02669e023eaafb3bac9e419fe43db
tdfprogramming <tdfprogramming@gmail.com>
2022-06-12 11:12:55 +0700

    add sec2

--- src/router/index.js
+++ src/router/index.js
@@ -18,9 +18,7 @@ const router = createRouter({
     },
     {
       path: "/undang",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      name: "undangan",
       component: Undangan,
     },
   ],
--- src/views/UndanganView.vue
+++ src/views/UndanganView.vue
@@ -1,27 +1,105 @@
 <script setup>
+import 'animate.css';
 
 </script>
 <template>
-  <!-- <main>
+  <main>
     <audio hidden controls autoplay="true">
       <source src="./../assets/music/music.mp3" type="audio/mpeg">
     </audio>
-    undang
-  </main> -->
+  </main>
+  <section>
+    <div class="bgaja bg-white py-14 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  rounded-md">
+        <div class="text-center ">
+          <p class="mt-4 max-w-2xl text-gray-500 lg:mx-auto">
+            "Dan di antara tanda-tanda kekuasaan-Nya ialah dia menciptakan isteri-isteri dari jenismu sendiri, supaya
+            kamu cenderung dan merasa tentram kepadanya, dan jadikan-Nya diantaramu rasa kasih dan sayang. Sesungguhnya
+            pada yang demikian itu benar-benar terdapat tanda-tanda bagi kamu yang berfikir.""
+            <br>
+            {Surat Ar-Rum Ayat 21}
+          </p>
+          <div class="flex justify-center ">
+            <img class="animate__animated animate__fadeInDown" src="./../assets/images/name.png" alt="" />
+          </div>
+          <p class="mt-2 leading-8 font-extrabold tracking-tight desc text-2xl">Sabtu, 25 Juni 2022</p>
+          <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Kediaman Mempelai Wanita</p>
 
-  <div class="py-12 bg-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="text-center">
-        <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
-        <p class="mt-2 leading-8 font-extrabold tracking-tight text-gray-900 text-4xl">A better way to send
-          money</p>
-        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Lorem ipsum dolor sit amet consect adipisicing elit.
-          Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+          <div class="relative rounded-xl overflow-auto p-8">
+            <div class="flex justify-center">
+              <div
+                class=" animate__animated animate__bounce animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+                <svg class="w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-
+  </section>
+  <section>
+    <div class="s bg-white py-14 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  rounded-md">
+        <div class="text-center ">
+          <p class="mt-4 max-w-2xl text-gray-500 lg:mx-auto">
+            <img width="600"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Basmala.svg/640px-Basmala.svg.png" alt="">
+            ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّٰهِ وَبَرَكَاتُهُ
+            <br> Seraya terngadah jemari dalam harapan kasih illahi, dengan memohon rahmat dan ridho Allah SWT
+            Perkenakan kami menikahkan putra-putri kami :
+          </p>
+          <br>
+          <div class="flex justify-center h-96  drop-shadow-md hover:drop-shadow-xl">
+            <img class="animate__animated animate__fadeInDown rounded-xl"
+              src="./../assets/images/cb624a81-333e-4ec4-941d-475ba8ca14e8.jpeg" alt="" />
+          </div>
+          <p class="mt-4 lg:mx-auto">
+            <span class="sec2">Melika Santi Mendrofa</span>
+            <br>
+            Putri dari Bapak Ide Siddik Mendrofa dan Ibu Ernawati Hasibuan
+            <br>
+          <div class="flex justify-center">
+            <img width="30" src="https://cdn-icons-png.flaticon.com/512/2904/2904973.png" alt="">
+          </div>
+          <span class="sec2 mt-4 lg:mx-auto"> Refki Ramadhani</span>
+          <br>
+          Putra dari Bapak Aries Chan dan Ibu Nelmy Yetti
+          <br>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+
+.bgaja {
+  background-image: url('https://nikahyo.com/image/wedding/background/bg-safitri-helmi.jpg');
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  background-size: cover;
+}
+
+.desc {
+  color: #715321;
+}
+
+h1 {
+  font-family: 'Dancing Script', cursive;
+  font-size: 3rem;
+}
+
+.sec2 {
+  font-family: 'Dancing Script', cursive;
+  font-weight: bold;
+  font-size: 2rem;
+}
 </style>
